c3b8f6a80d6dae72c49a352a62911b369d1f8863
Benjamin <Benjamin.valleix@gmail.com>
2023-02-11 22:44:36 +0100

    :pushpin: Fix Bug - CI passed

--- Sources/puissance4/Tests/puissance4Tests/Game_UT.swift
+++ Sources/puissance4/Tests/puissance4Tests/Game_UT.swift
@@ -4,7 +4,8 @@ import XCTest
 final class Game_UT: XCTestCase {
 
     func testInit() throws {
-        let board = Board(nbR: 5, nbC: 8)!
+        var board = Board(nbR: 6, nbC: 8)!
+        var game : Game?
         XCTAssertNotNil(board)
         let lecteur = Lecteur()
         XCTAssertNotNil(lecteur)
@@ -14,28 +15,28 @@ final class Game_UT: XCTestCase {
         XCTAssertNotNil(player1)
         let player2 = IA(withId: 2, andName: "IA", playIn: board)
         XCTAssertNotNil(player2)
-        func expect(board : Board, players: [Player], afficheur: Afficheur){
-            var game = Game(withBoard: board, playWith: players, display: afficheur)
-            XCTAssertNotNil(game)
-            XCTAssertNil(game?.getWinner())
-        }
-        expect(board: board, players: [player1, player2], afficheur: afficheur)
+        let regle = ClassicRules(inBoard: &board)
+        XCTAssertNotNil(regle)
+        game = Game(withBoard: &board, playWith: [player1, player2], display: afficheur, andRules: regle!)
+        XCTAssertNotNil(game)
+        XCTAssertNil(game?.getWinner())
     }
 
     func testTour() throws {
-        let board = Board(nbR: 5, nbC: 8)!
+        var board = Board(nbR: 6, nbC: 8)!
         let lecteur = Lecteur()
+        let regle = ClassicRules(inBoard: &board)
         let player1 = Human(withId: 1, andName: "Joueur 1", playIn: board, andReadWith: lecteur)
         let player2 = Human(withId: 2, andName: "Joueur 2", playIn: board, andReadWith: lecteur)
-        var game = Game(withBoard: board, playWith: [player1, player2], display: Afficheur())
+        var game = Game(withBoard: &board, playWith: [player1, player2], display: Afficheur(), andRules: regle!)
         XCTAssertNotNil(board)
         XCTAssertFalse(board.isFull())
-        game?.joueurSuivant()
-        XCTAssertEqual(1, game!.numeroJoueur)
-        game?.joueurSuivant()
-        XCTAssertEqual(0, game!.numeroJoueur)
-        game?.joueurSuivant()
-        XCTAssertEqual(1, game!.numeroJoueur)
+        game.joueurSuivant()
+        XCTAssertEqual(1, game.numeroJoueur)
+        game.joueurSuivant()
+        XCTAssertEqual(0, game.numeroJoueur)
+        game.joueurSuivant()
+        XCTAssertEqual(1, game.numeroJoueur)
     }
 
 }
